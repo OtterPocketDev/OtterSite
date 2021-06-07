08a3d73373244382cc7286af19eb9a4b6b294846
OtterPocketDev <OtterPocket@protonmail.com>
2021-06-07 12:26:05 -0600

    Analytics Script. Matomo

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,24 @@ export default {
   name: "App",
   mounted() {
     //  [App.vue specific] When App.vue is finish loading finish the progress bar
+    // MATOMO Analytics just to keep track of number of site visitors
+    var _paq = (window._paq = window._paq || []);
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(["trackPageView"]);
+    _paq.push(["enableLinkTracking"]);
+    (function() {
+      var u = "https://otterpocket.matomo.cloud/";
+      _paq.push(["setTrackerUrl", u + "matomo.php"]);
+      _paq.push(["setSiteId", "1"]);
+      var d = document,
+        g = d.createElement("script"),
+        s = d.getElementsByTagName("script")[0];
+      g.type = "text/javascript";
+      g.async = true;
+      g.src = "//cdn.matomo.cloud/otterpocket.matomo.cloud/matomo.js";
+      s.parentNode.insertBefore(g, s);
+    })();
+
     this.$Progress.finish();
     this.loader();
   },
